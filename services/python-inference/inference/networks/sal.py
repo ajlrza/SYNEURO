935e73d7bf83206fda44f3d120c604b85c353404
aj <158788527+ajlrza@users.noreply.github.com>
2026-06-07 22:31:52 +0800

    Implement SALNetwork with attention management
    Refactor SALNetwork class to handle raw data and manage attention.

--- services/python-inference/inference/networks/sal.py
+++ services/python-inference/inference/networks/sal.py
@@ -1,3 +1,36 @@
+import contextRAG
+import CENNetwork
+
 class SALNetwork:
-     def __init__(self, DFMCENObject):
-          pass
+     environmentalData = []
+     attentionList = []
+     currentAttention = {}
+     
+     def __init__(self, rawData: any):
+          self.environmentalData.append(rawData)
+          # Get current status of user activity
+
+     def signalNetwork(self) -> None:
+          RAG = contextRAG(self.environmentalData)
+          for state, data in enumerate(RAG):
+               if (state == "Focus"):
+                    self.attentionList.append(data)
+                    
+     def divertAttention(self):
+          attentionCount = 0
+          taskCount = 0
+          CEN = CENNetwork()
+          
+          for attention in self.attentionList:
+               attentionCount += 1
+               currentAttention[attentionCount] = attention
+               
+          while (True):
+               CEN.pushAttention(self.attentionList[taskCount]
+               if (CEN.checkAttention == False):
+                   continue
+               else:
+                   attentionList.pop(taskCount)
+                   taskCount += 1
+               
+               
