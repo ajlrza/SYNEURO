08b5c70f8379e565b58239c699d374d0a85a1c94
Tenshi <158788527+basedTenshi@users.noreply.github.com>
2026-07-23 15:08:26 +0800

    fix spaces

--- services/go_engine/cmd/server/main.go
+++ services/go_engine/cmd/server/main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"strings"
-    "encoding/json" 
-	"github.com/gofiber/fiber/v3"
-	//"net/http"
+     "fmt"
+     "log"
+     "strings"
+     "encoding/json" 
+     "github.com/gofiber/fiber/v3"
+     //"net/http"
 )
 
 func main() {
-    
-    app := fiber.New()
+     
+     app := fiber.New()
 
-    //networkClient := &http.Client{}
+     //networkClient := &http.Client{}
 
-    // Define a route for the GET method on the root path '/'
-    app.Get("/", func(c fiber.Ctx) error {
-        // Send a string response to the client
+     app.Get("/", func(c fiber.Ctx) error {
         return c.SendString("Go Lang TEST 👋!")
-    })
+     })
 
-    app.Post("/interact", func(c fiber.Ctx) error {
+     app.Post("/interact", func(c fiber.Ctx) error {
         if !c.HasBody() {
             return c.SendStatus(fiber.StatusBadRequest)
         }
@@ -31,14 +29,14 @@ func main() {
 
     })
 
-    var memory_cache any
+     var memory_cache any
 
-    app.Post("/cache-memory", func(c fiber.Ctx) error {
+     app.Post("/cache-memory", func(c fiber.Ctx) error {
 
         if !c.HasBody() {
             return c.SendStatus(fiber.StatusBadRequest)
         }
-        
+     
         body := c.Body()
         u, err := url.Parse(c.FullURL())
         userId := ""
@@ -58,9 +56,8 @@ func main() {
 
         memory_cache = body
 
-
     })
 
-    log.Fatal(app.Listen(":3000"))
-    fmt.Println("Server sarted at https://localhost/3000")
+     log.Fatal(app.Listen(":3000"))
+     fmt.Println("Server sarted at https://localhost/3000")
 }
